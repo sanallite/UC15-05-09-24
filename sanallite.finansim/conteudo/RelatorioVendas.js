d9c84f11191212efc46f700727801df2982641ad
Teodoro <149595409+sanallite@users.noreply.github.com>
2024-09-02 11:52:16 -0300

    Arrays com os meses e seus dados

--- sanallite.finansim/conteudo/RelatorioVendas.js
+++ sanallite.finansim/conteudo/RelatorioVendas.js
@@ -14,90 +14,88 @@ export default function RelatorioVendas() {
     ];
 
     let janeiro = [
-        { setor: 'Celulares', resultados: 148259.33, numeroVendas: 40 },
-        { setor: 'Computadores', resultados: 123800.31, numeroVendas: 34 },
-        { setor: 'Assistência Técnica', resultados: 94750.43, numeroVendas: 22 },
-        { setor: 'Acessórios', resultados: 43546.05, numeroVendas: 14 }
+        { setor: 'Celulares', resultados: 148259.33, numeroVendas: 400 },
+        { setor: 'Computadores', resultados: 123800.31, numeroVendas: 340 },
+        { setor: 'Assistência Técnica', resultados: 94750.43, numeroVendas: 220 },
+        { setor: 'Acessórios', resultados: 43546.05, numeroVendas: 140 }
     ]
 
     let fevereiro = [
-        { setor: 'Celulares', resultados: 252579.52, numeroVendas: 74 },
-        { setor: 'Computadores', resultados: 356086.50, numeroVendas: 67 },
-        { setor: 'Assistência Técnica', resultados: 89567.06, numeroVendas: 34 },
-        { setor: 'Acessórios', resultados: 84704.73, numeroVendas: 24 }
+        { setor: 'Celulares', resultados: 252579.52, numeroVendas: 740 },
+        { setor: 'Computadores', resultados: 356086.50, numeroVendas: 670 },
+        { setor: 'Assistência Técnica', resultados: 89567.06, numeroVendas: 340 },
+        { setor: 'Acessórios', resultados: 84704.73, numeroVendas: 240 }
     ]
 
     let marco = [
-        { setor: 'Celulares', resultados: 466842.31, numeroVendas: 45 },
-        { setor: 'Computadores', resultados: 424856.74, numeroVendas: 58 },
-        { setor: 'Assistência Técnica', resultados: 94750.43, numeroVendas: 22 },
-        { setor: 'Acessórios', resultados: 43546.05, numeroVendas: 14 }
+        { setor: 'Celulares', resultados: 466842.31, numeroVendas: 450 },
+        { setor: 'Computadores', resultados: 424856.74, numeroVendas: 580 },
+        { setor: 'Assistência Técnica', resultados: 54893.21, numeroVendas: 760 },
+        { setor: 'Acessórios', resultados: 74651.42, numeroVendas: 520 }
     ]
 
     let abril = [
-        { setor: 'Celulares', resultados: 148259.33, numeroVendas: 40 },
-        { setor: 'Computadores', resultados: 123800.31, numeroVendas: 34 },
-        { setor: 'Assistência Técnica', resultados: 94750.43, numeroVendas: 22 },
-        { setor: 'Acessórios', resultados: 43546.05, numeroVendas: 14 }
+        { setor: 'Celulares', resultados: 724251.00, numeroVendas: 870 },
+        { setor: 'Computadores', resultados: 764124.11, numeroVendas: 420 },
+        { setor: 'Assistência Técnica', resultados: 346745.14, numeroVendas: 670 },
+        { setor: 'Acessórios', resultados: 75411.14, numeroVendas: 120 }
     ]
 
     let maio = [
-        { setor: 'Celulares', resultados: 148259.33, numeroVendas: 40 },
-        { setor: 'Computadores', resultados: 123800.31, numeroVendas: 34 },
-        { setor: 'Assistência Técnica', resultados: 94750.43, numeroVendas: 22 },
-        { setor: 'Acessórios', resultados: 43546.05, numeroVendas: 14 }
+        { setor: 'Celulares', resultados: 174879.42, numeroVendas: 750 },
+        { setor: 'Computadores', resultados: 751213.34, numeroVendas: 910 },
+        { setor: 'Assistência Técnica', resultados: 721364.42, numeroVendas: 950 },
+        { setor: 'Acessórios', resultados: 97514.11, numeroVendas: 460 }
     ]
 
     let junho = [
-        { setor: 'Celulares', resultados: 148259.33, numeroVendas: 40 },
-        { setor: 'Computadores', resultados: 123800.31, numeroVendas: 34 },
-        { setor: 'Assistência Técnica', resultados: 94750.43, numeroVendas: 22 },
-        { setor: 'Acessórios', resultados: 43546.05, numeroVendas: 14 }
+        { setor: 'Celulares', resultados: 324789.74, numeroVendas: 590 },
+        { setor: 'Computadores', resultados: 375671.21, numeroVendas: 640 },
+        { setor: 'Assistência Técnica', resultados: 37467.42, numeroVendas: 370 },
+        { setor: 'Acessórios', resultados: 34568.41, numeroVendas: 310 }
     ]
 
     let julho = [
-        { setor: 'Celulares', resultados: 148259.33, numeroVendas: 40 },
-        { setor: 'Computadores', resultados: 123800.31, numeroVendas: 34 },
-        { setor: 'Assistência Técnica', resultados: 94750.43, numeroVendas: 22 },
-        { setor: 'Acessórios', resultados: 43546.05, numeroVendas: 14 }
+        { setor: 'Celulares', resultados: 367894.12, numeroVendas: 320 },
+        { setor: 'Computadores', resultados: 387122.54, numeroVendas: 605 },
+        { setor: 'Assistência Técnica', resultados: 87987.21, numeroVendas: 620 },
+        { setor: 'Acessórios', resultados: 687896.71, numeroVendas: 550 }
     ]
 
     let agosto = [
-        { setor: 'Celulares', resultados: 148259.33, numeroVendas: 40 },
-        { setor: 'Computadores', resultados: 123800.31, numeroVendas: 34 },
-        { setor: 'Assistência Técnica', resultados: 94750.43, numeroVendas: 22 },
-        { setor: 'Acessórios', resultados: 43546.05, numeroVendas: 14 }
+        { setor: 'Celulares', resultados: 765721.11, numeroVendas: 400 },
+        { setor: 'Computadores', resultados: 348744.12, numeroVendas: 390 },
+        { setor: 'Assistência Técnica', resultados: 54876.41, numeroVendas: 402 },
+        { setor: 'Acessórios', resultados: 641221.74, numeroVendas: 890 }
     ]
 
     let setembro = [
-        { setor: 'Celulares', resultados: 148259.33, numeroVendas: 40 },
-        { setor: 'Computadores', resultados: 123800.31, numeroVendas: 34 },
-        { setor: 'Assistência Técnica', resultados: 94750.43, numeroVendas: 22 },
-        { setor: 'Acessórios', resultados: 43546.05, numeroVendas: 14 }
+        { setor: 'Celulares', resultados: 374897.41, numeroVendas: 580 },
+        { setor: 'Computadores', resultados: 98712.21, numeroVendas: 470 },
+        { setor: 'Assistência Técnica', resultados: 89427.44, numeroVendas: 320 },
+        { setor: 'Acessórios', resultados: 67136.74, numeroVendas: 480 }
     ]
 
     let outubro = [
-        { setor: 'Celulares', resultados: 148259.33, numeroVendas: 40 },
-        { setor: 'Computadores', resultados: 123800.31, numeroVendas: 34 },
-        { setor: 'Assistência Técnica', resultados: 94750.43, numeroVendas: 22 },
-        { setor: 'Acessórios', resultados: 43546.05, numeroVendas: 14 }
+        { setor: 'Celulares', resultados: 489721.21, numeroVendas: 370 },
+        { setor: 'Computadores', resultados: 597213.57, numeroVendas: 590 },
+        { setor: 'Assistência Técnica', resultados: 57542.12, numeroVendas: 440 },
+        { setor: 'Acessórios', resultados: 45794.21, numeroVendas: 260 }
     ]
 
     let novembro = [
-        { setor: 'Celulares', resultados: 148259.33, numeroVendas: 40 },
-        { setor: 'Computadores', resultados: 123800.31, numeroVendas: 34 },
-        { setor: 'Assistência Técnica', resultados: 94750.43, numeroVendas: 22 },
-        { setor: 'Acessórios', resultados: 43546.05, numeroVendas: 14 }
+        { setor: 'Celulares', resultados: 1567489.41, numeroVendas: 510 },
+        { setor: 'Computadores', resultados: 3678421.53, numeroVendas: 990 },
+        { setor: 'Assistência Técnica', resultados: 489421.11, numeroVendas: 404 },
+        { setor: 'Acessórios', resultados: 675421.75, numeroVendas: 850 }
     ]
 
     let dezembro = [
-        { setor: 'Celulares', resultados: 148259.33, numeroVendas: 40 },
-        { setor: 'Computadores', resultados: 123800.31, numeroVendas: 34 },
-        { setor: 'Assistência Técnica', resultados: 94750.43, numeroVendas: 22 },
-        { setor: 'Acessórios', resultados: 43546.05, numeroVendas: 14 }
+        { setor: 'Celulares', resultados: 875512.10, numeroVendas: 610 },
+        { setor: 'Computadores', resultados: 675142.01, numeroVendas: 570 },
+        { setor: 'Assistência Técnica', resultados: 278942.15, numeroVendas: 280 },
+        { setor: 'Acessórios', resultados: 155472.31, numeroVendas: 250 }
     ]
-
-    /* Continue implementando */
 
     return (
         <View style={ estilo.relatorios }>
